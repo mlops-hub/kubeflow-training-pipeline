bb0a1f33b3315dd8857ec09daaa937cefb358121
SravyaCrunchOps <sravya@crunchops.com>
2026-01-31 04:49:15 +0530

    local_and_s3 configuration

--- src/data_pipeline/_01_ingestion.py
+++ src/data_pipeline/_01_ingestion.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+DATASET_PATH = BASE_DIR / "datasets"
+RAW_DATA_PATH = DATASET_PATH / "employee_attrition.csv"
+INGESTION_PATH = DATASET_PATH / "data-pipeline" / "01_ingestion.csv"
+os.makedirs(INGESTION_PATH.parent, exist_ok=True)
 
 def ingestion() -> pd.DataFrame:
     df = pd.read_csv(RAW_DATA_PATH)
@@ -14,14 +22,6 @@ def ingestion() -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    import os
-    from pathlib import Path
-
-    BASE_DIR = Path(__file__).resolve().parents[2]
-    DATASET_PATH = BASE_DIR / "datasets"
-    RAW_DATA_PATH = DATASET_PATH / "employee_attrition.csv"
-    INGESTION_PATH = DATASET_PATH / "data-pipeline" / "01_ingestion.csv"
-    os.makedirs(INGESTION_PATH.parent, exist_ok=True)
 
     df = ingestion()
 
